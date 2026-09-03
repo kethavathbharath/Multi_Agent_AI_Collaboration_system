334b1aa1b5ba2ac70b79f131f99e725b059aced3
Rishika583 <poreddyrishika0@gmail.com>
2026-09-03 06:41:22 +0530

    Update app.py

--- frontend/app.py
+++ frontend/app.py
@@ -20,7 +20,7 @@ st.set_page_config(
 # BACKEND
 # ============================================================
 
-BACKEND_URL = https://multi-agent-ai-collaboration-system-3.onrender.com
+BACKEND_URL = "https://multi-agent-ai-collaboration-system-3.onrender.com"
 
 
 # ============================================================
